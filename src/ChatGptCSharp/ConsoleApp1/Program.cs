7b6c3b23af7088f9cb1a62cf9325ef6302b004e4
Usman <usman.solemn@gmail.com>
2023-02-08 11:34:58 +0500

    Chat GPT integration using c#

--- src/ChatGptCSharp/ConsoleApp1/Program.cs
+++ src/ChatGptCSharp/ConsoleApp1/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace ConsoleApp1
+﻿using OpenAI.GPT3.Managers;
+using OpenAI.GPT3;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels;
+using Betalgo.GPT3.Based.Console;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Setup.Configure();
+
+            var apiKey = Environment.GetEnvironmentVariable("ApiKey");
+
+            var openAiService = new OpenAIService(new OpenAiOptions()
+            {
+                ApiKey = apiKey
+            });
+
+            var completionResult = await openAiService.Completions
+                .CreateCompletion(new CompletionCreateRequest()
+            {
+                Prompt = "What is a programming language?",
+                Model = Models.TextDavinciV2,
+                MaxTokens = 4000
+            });
+
+            if (completionResult.Successful)
+            {
+                foreach(var choice in completionResult.Choices) {
+                    Console.WriteLine(choice);
+                }
+            }
+            else
+            {
+                if (completionResult.Error == null)
+                {
+                    throw new Exception("Unknown Error");
+                }
+                Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+            }
         }
     }
 }
---
+++ src/ChatGptCSharp/ConsoleApp1/Setup.cs
@@ -0,0 +1,10 @@
+﻿namespace Betalgo.GPT3.Based.Console
+{
+    internal class Setup
+    {
+        public static void Configure()
+        {
+            Environment.SetEnvironmentVariable("ApiKey", "");
+        }
+    }
+}
